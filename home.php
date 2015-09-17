199bfb1378c67d9fd42e1643320cc1025c98780f
vincseize <vincseize@BORGES>
2015-09-17 15:08:41 +0200

    Preparation intégration Webix dans UI Layout

--- home.php
+++ home.php
@@ -1,209 +1,54 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+<!DOCTYPE html>
 <html>
-<head>
-	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-
-
-	<title>SaAM V2</title>
-
-
-
-
-	<link type="text/css" rel="stylesheet" href="css/layout-default-latest.css" />
-	<style type="text/css">
-
-
-
-	</style>
-
-  	<link rel="stylesheet" href="css/bootstrap.min.css">	<!-- order 1,2,3 IMPORTANT -->
-  	<script src="js/jquery.min.js"></script>  				<!-- order 1,2,3 IMPORTANT -->
-  	<script src="js/bootstrap.min.js"></script>  			<!-- order 1,2,3 IMPORTANT -->
-
-
-
-
-	<script type="text/javascript" src="js/jquery-latest.js"></script>
-	<script type="text/javascript" src="js/jquery-ui-latest.js"></script>
-	<script type="text/javascript" src="js/jquery.layout-latest.js"></script>
-	<script type="text/javascript" src="js/complex.js"></script>
-	<script type="text/javascript" src="js/debug.js"></script>
-	<script type="text/javascript">
-
-
-	
-
-
-	var layoutOptions = {
-	
-	
-	
-		center__childOptions: {
-			inset: {
-				top:	0
-			,	bottom:	0
-			,	left:	0
-			,	right:	0
-			}
-		}
-	};
-
-	
-	
-	
-	
-
-	
-	
-	
-	
-	
-	$(document).ready(function(){
-	
-		// var outerLayout, innerLayout;	
-	
-		// outerLayout = $("body").layout( layoutSettings_Outer );	
-	
-		$('body').layout( layoutOptions );
-		// var myLayout = $("body").layout();
-		// var innerLayout = $("div.pane-center").layout( layoutSettings_Inner );
-		// myLayout.close('north');
-		
-		
-		// myLayout.close('east');
-		// myLayout.close('west');
-		// myLayout.close('south');
-		
-		
-		
-		//myLayout.state.south.close('south');
-		
-		// myLayout.togglerLength_open(0);			// HIDE the toggler button
-		//myLayout.hide('south')	
-		
-		// $( ".ui-layout-west" ).remove();
-		
-		
-		
-		
-		
-		
-	      var layoutSettings_Outer = {		
-		    name: "outerLayout" // NO FUNCTIONAL USE, but could be used by custom code to 'identify' a layout	
-	      
-	      
-		    ,	defaults: {
-			      size:					"auto"
-
-	      }	
-	  
-	
-		/*
-	*#######################
-	* INNER LAYOUT SETTINGS
-	*#######################
-	*
-	* These settings are set in 'list format' - no nested data-structures
-	* Default settings are specified with just their name, like: fxName:"slide"
-	* Pane-specific settings are prefixed with the pane name + 2-underscores: north__fxName:"none"
-
-var layoutSettings_Inner = {
-			size:					"auto"
-	};
-	
-	
-	
-		*/
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-			};
-		
-		
-		
-		
-		
-		
-	});
-
-	
-
-	
-	</script>
-
-</head>
-<body>
-
-
-
-
-
-<?php include('navbar.php'); ?>
-
-
-
-
-
-
-
-
-<div id="menu-north"  class="ui-layout-north" style="background-color:#444444;"><?php include('menu_top_home.php'); ?></div>
-<div id="menu-west"  class="ui-layout-west" style="background-image: url('tmp/bg_menu_left.jpg');background-repeat: no-repeat;background-size: cover;"></div>
-<div id="menu-east"  class="ui-layout-east"	style="background-image: url('tmp/bg_admin_right.jpg');background-repeat: no-repeat;background-size: cover;"></div>
-<div id="menu-south"  class="ui-layout-south" style="background-color:#111;"><font color='white'>Outer South (MESSAGES, AUTRES ...), contenu changeant</font></div>
-
-
-
-<div id="content" class="ui-layout-center center" style"padding:0;margin:0;size:auto;">
-<?php include('home_content.php'); ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-</div>
-
-
-
-
-
-
-
-</body>
+   <head>
+       <link rel="stylesheet" href="js/webix.css" type="text/css" media="screen" charset="utf-8">
+       <script src="js/webix.js" type="text/javascript" charset="utf-8"></script>
+       <title>Layout and Resizer</title>
+   </head>
+   <body>
+       <div id='layout_div' style='width:600px; height:600px; margin:20px;'></div>
+       <script type="text/javascript" charset="utf-8">
+
+           webix.ui({
+               container:"layout_div",
+               id:"layout",
+               height:550,
+               width:700,
+               rows:[
+                   {template:"row 1", height:35 },
+                   {template:"row 1"},
+                   {view:"resizer"},
+                   {template:"row 2"},
+                   {view:"resizer"},
+                   {cols:[
+                       {
+                           id:"a1",
+                           template:"column 1",
+                           width:150
+                       },
+                       {
+                           view:"resizer",
+                           id:"resizer"
+                       },
+                       {   
+                           template:"column 2"
+                       },
+                       {
+                           template:"column 3"
+                       }
+                   ]
+                   }
+               ]
+           }).show();
+
+       </script>
+   </body>
 </html>
