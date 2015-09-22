084f58528391e4da974d2f49e1088bf8335c2a50
vincseize <vincseize@BORGES>
2015-09-23 01:59:03 +0200

    Home Menu Tips

--- menus/home/left.php
+++ menus/home/left.php
@@ -19,15 +19,16 @@ webix.ready(function(){
 		view:"tree",
 		select:true,
 		data: [
-   			{id:"root", value:"Project Demo", open:true, data:[
-				{ id:"1", open:true, value:"Sequence 1", data:[
-					{ id:"1.1", value:"Plan 1" },
-					{ id:"1.2", value:"Plan 2" },
-					{ id:"1.3", value:"Plan 3" }
+   			{id:"root", value:"Tips", open:true, data:[
+				{ id:"1", open:true, value:"help", data:[
+					{ id:"1.1", value:"b 1" },
+					{ id:"1.2", value:"b 2" },
+					{ id:"1.3", value:"b 3" }
 				]},
-				{ id:"2", value:"Sequence 2", open:true, data:[
-					{ id:"2.1", value:"Plan 1" },
-					{ id:"2.2", value:"Plan 2" }
+				{ id:"2", value:"random tips", open:true, data:[
+					{ id:"2.1", value:"dpt 1" },
+					{ id:"2.2", value:"dpt 2" },
+                                        { id:"2.3", value:"dpt x" }
 				]}
 			]}
 		]
